04cbbf5038362fa7b688e86c7c8bc2a6ddf5342f
Ubuntu <ubuntu@ip-172-31-2-9.us-west-2.compute.internal>
2024-09-10 05:37:01 +0000

    Added diffusion action head

--- prismatic/vla/action_head.py
+++ prismatic/vla/action_head.py
@@ -9,6 +9,8 @@ from typing import Optional
 import torch
 import torch.nn as nn
 
+from diffusion import cosine_beta_schedule, create_diffusion_model
+
 
 # === Definitions for Various Action Heads === #
 class LinearActionHead(nn.Module):
@@ -18,7 +20,7 @@ class LinearActionHead(nn.Module):
             action_dim: int, 
             use_map: bool, 
             num_map_heads: Optional[int] = None
-            ) -> None:
+        ) -> None:
         super().__init__()
         self.action_head = nn.Linear(llm_dim, action_dim, bias=True)
         self.use_map = use_map
@@ -44,7 +46,7 @@ class MLPActionHead(nn.Module):
             use_map: bool, 
             num_map_heads: Optional[int] = None, 
             mlp_type: str = "gelu"
-            ) -> None:
+        ) -> None:
         super().__init__()
         if mlp_type == "gelu":
             self.action_head = nn.Sequential(
@@ -73,6 +75,115 @@ class MLPActionHead(nn.Module):
             pooled_output = llm_output.mean(dim=1)
 
         return self.action_head(pooled_output)
+
+
+class DiffusionActionHead(nn.Module):
+    def __init__(
+            self,
+            llm_dim: int,
+            time_dim: int = 32,
+            num_blocks: int = 3,
+            hidden_dim: int = 256,
+            use_layer_norm: bool = True,
+            diffusion_steps: int = 20,
+            n_diffusion_samples: int = 1,
+            use_map: bool = True,
+            num_map_heads: Optional[int] = None,
+        ) -> None:
+
+        self.diffusion_steps = diffusion_steps
+        self.n_diffusion_samples = n_diffusion_samples
+        self.use_map = use_map
+
+        if use_map:
+            assert num_map_heads is not None, "Should pass in number of attention heads if 'use_map' is True!"
+            self.map_head = MAPHead(llm_dim=llm_dim, num_heads=num_map_heads)
+
+        # create the diffusion model (score network)
+        self.diffusion_model = create_diffusion_model(
+            llm_dim=llm_dim,
+            out_dim=7,
+            time_dim=time_dim,
+            num_blocks=num_blocks,
+            hidden_dim=hidden_dim,
+            use_layer_norm=use_layer_norm,
+        )
+
+        # create beta schedule
+        self.betas = torch.tensor(cosine_beta_schedule(diffusion_steps), dtype=torch.float32)
+        self.alphas = 1.0 - self.betas
+        self.alpha_hats = torch.cumprod(self.alphas, dim=0)
+
+    def forward(
+            self, 
+            llm_output: torch.Tensor,
+            time: Optional[torch.Tensor] = None,
+            noisy_actions: Optional[torch.Tensor] = None,
+        ) -> torch.Tensor:
+
+        if self.use_map:
+            pooled_output = self.map_head(llm_output)
+        else:
+            pooled_output = llm_output.mean(dim=1)
+
+        # time and noisy_actions are None during initialization, so we replace them with a dummy array
+        if time is None or noisy_actions is None:
+            if not self.training:
+                raise ValueError("Must provide time and noisy_actions when calling diffusion action head")
+            else:
+                time = torch.zeros((pooled_output.shape[0], 1), dtype=torch.float32)
+                noisy_actions = torch.zeros((pooled_output.shape[0], 7), dtype=torch.float32)
+
+        pred_eps = self.diffusion_model(pooled_output, noisy_actions, time)
+        return pred_eps
+
+    def loss(self, llm_output: torch.Tensor, gt_actions: torch.Tensor, seed: int) -> torch.Tensor:
+        """
+        Computes the loss for the diffusion objective.
+        """
+        batch_size = llm_output.shape[0]
+
+        # Generate random time and noise
+        rng = torch.Generator().manual_seed(seed)
+        time = torch.randint(0, self.diffusion_steps, (self.n_diffusion_samples, batch_size, 1), generator=rng)
+        noise = torch.randn((self.n_diffusion_samples, batch_size, 7), generator=rng)
+
+        # Add noise to the action according to the schedule
+        scale = torch.sqrt(self.alpha_hats[time])
+        std = torch.sqrt(1 - self.alpha_hats[time])
+        noisy_actions = scale * gt_actions.unsqueeze(0) + std * noise
+
+        pred_eps = self(llm_output, time=time, noisy_actions=noisy_actions)
+        loss = torch.nn.functional.mse_loss(pred_eps, noise)
+
+        return loss
+
+    def predict_action(self, llm_output: torch.Tensor, seed: int = 7):
+        rng = torch.Generator().manual_seed(seed)
+        batch_size = llm_output.shape[0]
+        assert batch_size == 1
+
+        def scan_fn(current_x: torch.Tensor, time: torch.Tensor):
+            input_time = time.expand(size=(batch_size, 1))
+
+            eps_pred = self(llm_output, time=input_time, noisy_actions=current_x)
+
+            alpha_1 = 1.0 / torch.sqrt(self.alphas[time])
+            alpha_2 = (1.0 - self.alphas[time]) / (torch.sqrt(1.0 - self.alpha_hats[time]))
+            current_x = alpha_1 * (current_x - alpha_2 * eps_pred)
+
+            z = torch.randn(size=current_x.shape, generator=rng)
+            current_x = current_x + (time > 0) * (torch.sqrt(self.betas[time]) * z)
+
+            return current_x
+
+        noise = torch.randn(size=(batch_size, 7), generator=rng)
+        actions = noise
+
+        for t in reversed(range(self.diffusion_steps)):
+            actions = scan_fn(actions, torch.tensor(t, dtype=torch.long))
+
+        return actions[0]
 
 
 # === Definitions for Multi-Head Attention Pooling === #
---
+++ prismatic/vla/diffusion.py
@@ -0,0 +1,187 @@
+from typing import Sequence, Callable
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+
+
+def cosine_beta_schedule(timesteps: int, s: float = 0.008):
+    """
+    cosine schedule
+    as proposed in https://openreview.net/forum?id=-NEXDKk8gZ
+    """
+    steps = timesteps + 1
+    t = np.linspace(0, timesteps, steps) / timesteps
+    alphas_cumprod = np.cos((t + s) / (1 + s) * np.pi * 0.5) ** 2
+    alphas_cumprod = alphas_cumprod / alphas_cumprod[0]
+    betas = 1 - (alphas_cumprod[1:] / alphas_cumprod[:-1])
+    return np.clip(betas, 0, 0.999)
+
+
+class FourierFeatures(nn.Module):
+    def __init__(self, output_size: int, learnable: bool = True):
+        super().__init__()
+        self.learnable = learnable
+
+        if learnable:
+            self.kernel = nn.Parameter(torch.randn(output_size // 2, 1) * 0.2)
+        else:
+            half_dim = output_size // 2
+            self.f = np.log(10000) / (half_dim - 1)
+            self.f = torch.exp(-self.f * torch.arange(half_dim).float())
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        assert x.shape[-1] == 1
+        if self.learnable:
+            f = 2 * np.pi * x @ self.kernel.T
+        else:
+            f = x * self.f
+        return torch.cat([torch.cos(f), torch.sin(f)], dim=-1)
+
+
+class MLP(nn.Module):
+    def __init__(
+            self,
+            input_dim: int,
+            hidden_dims: Sequence[int], 
+            activation: Callable = F.silu,
+            activate_final: bool = False, 
+            use_layer_norm: bool = False,
+        ):
+        super().__init__()
+        self.input_dim = input_dim
+        self.layers = nn.ModuleList()
+
+        for i, size in enumerate(hidden_dims):
+            self.layers.append(nn.Linear(hidden_dims[i - 1] if i > 0 else input_dim, size))
+
+            if i + 1 < len(hidden_dims) or activate_final:
+                if use_layer_norm:
+                    self.layers.append(nn.LayerNorm(size))
+                self.layers.append(activation)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        assert x.shape[-1] == self.input_dim
+        for layer in self.layers:
+            x = layer(x)
+        return x
+
+
+class MLPResNetBlock(nn.Module):
+    def __init__(
+            self,
+            features: int, 
+            activation: Callable = F.silu, 
+            use_layer_norm: bool = False,
+        ):
+        super().__init__()
+        self.features = features
+        self.activation = activation
+        self.use_layer_norm = use_layer_norm
+
+        self.fc1 = nn.Linear(features, features * 4)
+        self.fc2 = nn.Linear(features * 4, features)
+
+        if use_layer_norm:
+            self.layer_norm = nn.LayerNorm(features)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        assert x.shape[-1] == self.features
+        residual = x
+
+        if self.use_layer_norm:
+            x = self.layer_norm(x)
+        x = self.activation(self.fc1(x))
+        x = self.fc2(x)
+
+        return residual + x
+
+
+class MLPResNet(nn.Module):
+    def __init__(
+            self,
+            input_dim: int,
+            num_blocks: int, 
+            out_dim: int, 
+            hidden_dim: int = 256, 
+            activation: Callable = F.silu, 
+            use_layer_norm: bool = False,
+        ):
+        super().__init__()
+        self.input_dim = input_dim
+        self.activation = activation
+
+        self.fc = nn.Linear(input_dim, hidden_dim)
+        self.blocks = nn.ModuleList([
+            MLPResNetBlock(features=hidden_dim, activation=activation, use_layer_norm=use_layer_norm)
+            for _ in range(num_blocks)
+        ])
+        self.out_fc = nn.Linear(hidden_dim, out_dim)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        assert x.shape[-1] == self.input_dim
+
+        x = self.fc(x)
+        for block in self.blocks:
+            x = block(x)
+        return self.out_fc(self.activation(x))
+
+
+class ScoreActor(nn.Module):
+    def __init__(
+            self, 
+            time_preprocess: nn.Module, 
+            cond_encoder: nn.Module, 
+            reverse_network: nn.Module
+        ):
+        super().__init__()
+        self.time_preprocess = time_preprocess
+        self.cond_encoder = cond_encoder
+        self.reverse_network = reverse_network
+
+    def forward(
+            self, 
+            obs_enc: torch.Tensor, 
+            actions: torch.Tensor, 
+            time: torch.Tensor,
+        ) -> torch.Tensor:
+        """
+        Args:
+            obs_enc: (bd..., obs_dim) where bd... is broadcastable to batch_dims
+            actions: (batch_dims..., action_dim)
+            time: (batch_dims..., 1)
+        """
+        t_ff = self.time_preprocess(time)
+        cond_enc = self.cond_encoder(t_ff)
+        
+        if obs_enc.shape[:-1] != cond_enc.shape[:-1]:
+            new_shape = cond_enc.shape[:-1] + (obs_enc.shape[-1],)
+            obs_enc = obs_enc.expand(new_shape)
+
+        reverse_input = torch.cat([cond_enc, obs_enc, actions], dim=-1)
+        eps_pred = self.reverse_network(reverse_input)
+        return eps_pred
+
+
+def create_diffusion_model(
+    llm_dim: int,
+    out_dim: int, 
+    time_dim: int, 
+    num_blocks: int, 
+    hidden_dim: int,
+    use_layer_norm: bool,
+) -> nn.Module:
+    return ScoreActor(
+        time_preprocess=FourierFeatures(time_dim, learnable=True),
+        cond_encoder=MLP(
+            input_dim=time_dim,
+            hidden_dims=[2 * time_dim, time_dim]),
+        reverse_network=MLPResNet(
+            input_dim=time_dim+llm_dim+7,
+            num_blocks=num_blocks,
+            out_dim=out_dim,
+            hidden_dim=hidden_dim,
+            use_layer_norm=use_layer_norm,
+        ),
+    )
